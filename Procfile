a289f228007e7471f1601aa91b51438e46afad36
Ravit <dvirravi@gmail.com>
2022-05-10 10:57:56 +0300

    full_recipe_by_id - edit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn recipes_and_more_project.wsgi --log-file -
--- recipes_and_more_project/settings.py
+++ recipes_and_more_project/settings.py
@@ -139,3 +139,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
